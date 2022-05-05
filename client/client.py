22ca94cee5371663a029b1cadc28b73d491add3d
cblack389 <negruclaudiu389@yahoo.com>
2022-05-05 09:13:54 -0400

    basic client with requests

---
+++ client/client.py
@@ -0,0 +1,44 @@
+import PySimpleGUI as sg
+import requests
+
+def upload_file(path: str):
+    file = open(path, "rb")
+    to_upload = {"file": file}
+    response = requests.post(url="localhost:20000/upload", files=to_upload)
+
+    print(response.text)
+
+def device_status():
+    response = requests.get(url="localhost:20000/status", params={"deviceid": "aaa"})
+
+    print(response.text)
+
+def main_loop():
+    layout = [[sg.Text('Enter file path:')],      
+                 [sg.InputText()],      
+                 [sg.Submit(), sg.Cancel()],
+                 [sg.Button("Device status")]]      
+
+    window = sg.Window('Window Title', layout)    
+
+    while True:
+        event, values = window.read()    
+        print(values)
+
+        if event == sg.WIN_CLOSED:
+            break
+        elif event == "Submit":
+            print("Submit pressed")
+            upload_file(values[0])
+
+        elif event == "Cancel":
+            print("Cancel pressed")
+        elif event == "Device status":
+            print("Device status pressed")
+            device_status()
+        
+        
+    window.close()
+
+if __name__ == "__main__":
+    main_loop()
