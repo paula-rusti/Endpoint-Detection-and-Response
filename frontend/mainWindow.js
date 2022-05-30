4cf44a63ca253343eebca8f3cf9204c7cefb0714
mrsaibaandreas <andreasqp@gmail.com>
2022-05-30 23:32:47 +0300

    modified mainWindow.js

--- frontend/mainWindow.js
+++ frontend/mainWindow.js
@@ -5,6 +5,8 @@ const os = require('os');
 // we need ipcRenderer to communicate with main process
 const {ipcRenderer} = electron;
 
+
+//here mainly the buttons will be configured
 // axios config
 axios.defaults.baseURL = 'http://localhost:8005';
 
@@ -49,6 +51,7 @@ statusButton.addEventListener('click', event => {
 });
 
 // update device info elements
+//the os will everytime be verified
 const platformHeader = document.getElementById('platform_header');
 platformHeader.textContent = 'Platform: ' + os.platform();
 
